21c9e39ba2162847d55c294c26fce3ff63226fff
LarisaNeagos <neagos.larisa@gmail.com>
2018-05-08 14:31:00 +0300

    First assigment

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -10,6 +10,28 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int number = random.Next(1, 100);
+            int userGuess = 0;
+
+            Console.WriteLine("Please guess a number between 1 and 100.");
+
+            while (userGuess != number)
+            {
+                userGuess = Convert.ToInt32(Console.ReadLine());
+
+                if (userGuess < number)
+                {
+                    Console.WriteLine("Your number is too small, try again");
+                }
+                else if (userGuess > number)
+                {
+                    Console.WriteLine("Your number is too big, try again");
+                }
+            }
+
+            Console.WriteLine($"Congratulations, you guessed the number: {number}");
+            Console.ReadLine();
         }
     }
 }
